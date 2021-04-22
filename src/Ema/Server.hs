ec7014c69b0934608cb45ca59319dd2b22036115
Sridhar Ratnakumar <srid@srid.ca>
2021-04-22 13:34:44 -0400

    Iron out some bugs

--- src/Ema/Server.hs
+++ src/Ema/Server.hs
@@ -57,7 +57,7 @@ runServerWithWebSocketHotReload port model render = do
                 Left newHtml -> do
                   -- The page the user is currently viewing has changed. Send
                   -- the new HTML to them.
-                  WS.sendTextData conn $ render newHtml watchingRoute
+                  WS.sendTextData conn $ renderWithEmaHtmlShims newHtml watchingRoute
                   log $ "[Watch]: ~~> " <> show watchingRoute
                   loop
                 Right nextRoute -> do
@@ -67,7 +67,7 @@ runServerWithWebSocketHotReload port model render = do
                   -- request immediately following this).
                   log $ "[Switch]: <~~ " <> show nextRoute
                   html <- LVar.get model
-                  WS.sendTextData conn $ render html nextRoute
+                  WS.sendTextData conn $ renderWithEmaHtmlShims html nextRoute
                   log $ "[Switch]: ~~> " <> show nextRoute
                   loop
         try loop >>= \case
@@ -80,11 +80,13 @@ runServerWithWebSocketHotReload port model render = do
         Nothing ->
           pure (H.status404, "No route")
         Just r -> do
-          let renderWithEmaShims m r' =
-                render m r' <> emaStatusHtml <> wsClientShim
           val <- LVar.get model
           pure (H.status200, renderWithEmaShims val r)
       f $ Wai.responseLBS status [(H.hContentType, "text/html")] v
+    renderWithEmaShims m r =
+      render m r <> emaStatusHtml <> wsClientShim
+    renderWithEmaHtmlShims m r =
+      render m r <> emaStatusHtml
     routeFromPathInfo =
       fromSlug . fmap (fromString . toString)
 
@@ -181,7 +183,6 @@ wsClientShim =
           }
 
           function handleRouteClicks(e) {
-              console.log("click");
               const origin = e.target.closest("a");
               if (origin) {
                 if (window.location.host === origin.host) {
@@ -203,6 +204,7 @@ wsClientShim =
           };
           ws.onclose = () => {
             console.log("ema: closed; reconnecting ..");
+            window.removeEventListener(`click`, handleRouteClicks);
             window.reloading();
             // Reconnect after 1s, which is typical time it takes for ghcid to reboot.
             // Then, retry in another 1s. Ideally we need an exponential retry logic.
