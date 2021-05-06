0448f0b4ed0479e3b3858551d7f39b75d0bbaefd
Sridhar Ratnakumar <srid@srid.ca>
2021-05-06 09:10:27 -0400

    Update nixpkgs (lvar is in nixpkgs now)

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 {
   description = "Ema project";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/8389dcb67d934ee72c1d1e7228d92be9b3f71bad";
+    nixpkgs.url = "github:nixos/nixpkgs/c5173fe20851def75a5676a69a0d09b9d24ae428";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-    lvar = {
-      url = "github:srid/lvar";
-      flake = false;
-    };
+    #lvar = {
+    #  url = "github:srid/lvar";
+    #  flake = false;
+    #};
   };
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
@@ -26,7 +26,7 @@
             root = ./.;
             withHoogle = false;
             overrides = self: super: with pkgs.haskell.lib; {
-              lvar = self.callCabal2nix "lvar" inputs.lvar { };
+              # lvar = self.callCabal2nix "lvar" inputs.lvar { };
             };
             modifier = drv:
               pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
