d8e14ab63a9b25f44591c38c929b04b957f1546d
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2021-05-22 13:55:43 -0400

    Update Ex02_Basic.hs

--- src/Ema/Example/Ex02_Basic.hs
+++ src/Ema/Example/Ex02_Basic.hs
@@ -1,11 +1,6 @@
 {-# LANGUAGE TypeApplications #-}
 
--- | A very simple site with routes, but based on dynamically changing values
---
--- The current time is computed in the server every second, and the resultant
--- generated HTML is automatically updated on the browser. This is only a demo;
--- usually we render HTML based on files on disk or something accessible outside
--- of the browser. More advanced examples will demonstrate that.
+-- | A very simple site with two routes, and HTML rendered using Blaze DSL
 module Ema.Example.Ex02_Basic where
 
 import Control.Concurrent (threadDelay)
