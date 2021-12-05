ad884e4b7c8a711751850dce0d7ea8c5f22466de
Sridhar Ratnakumar <srid@srid.ca>
2021-12-05 17:04:07 -0500

    Support M1 Mac

--- flake.nix
+++ flake.nix
@@ -24,6 +24,21 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        # https://github.com/NixOS/nixpkgs/issues/140774#issuecomment-976899227
+        m1MacHsBuildTools =
+          pkgs.haskellPackages.override {
+            overrides = self: super:
+              let
+                workaround140774 = hpkg: with pkgs.haskell.lib;
+                  overrideCabal hpkg (drv: {
+                    enableSeparateBinOutput = false;
+                  });
+              in
+              {
+                ghcid = workaround140774 super.ghcid;
+                ormolu = workaround140774 super.ormolu;
+              };
+          };
         emaProject = returnShellEnv:
           pkgs.haskellPackages.developPackage {
             inherit name returnShellEnv;
@@ -34,15 +49,18 @@
               unionmount = self.callCabal2nix "unionmount" inputs.unionmount { };
             };
             modifier = drv:
-              pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
-              [
-                cabal-install
-                cabal-fmt
-                pkgs.nixpkgs-fmt
-                ghcid
-                ormolu
-                haskell-language-server
-              ]);
+              pkgs.haskell.lib.addBuildTools drv
+                (with (if system == "aarch64-darwin"
+                then m1MacHsBuildTools
+                else pkgs.haskellPackages); [
+                  # Specify your build/dev dependencies here. 
+                  cabal-fmt
+                  cabal-install
+                  ghcid
+                  haskell-language-server
+                  ormolu
+                  pkgs.nixpkgs-fmt
+                ]);
           };
         ema = emaProject false;
       in
