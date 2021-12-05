746479ce1b4466cd96b2eb195e648ccab82d8093
Sridhar Ratnakumar <srid@srid.ca>
2021-12-05 16:58:09 -0500

    Advance nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Ema project";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/56cbe42f1668338d05febfbb866e32f2c865609a";
+    nixpkgs.url = "github:nixos/nixpkgs/2df15ba83d0510a56f2583fd3481723835acb5a1";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
