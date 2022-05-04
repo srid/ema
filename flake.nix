a8e344c0e82abf362d7f4c157c8ccb7f6c776055
Sridhar Ratnakumar <srid@srid.ca>
2022-05-04 19:54:57 -0400

    Update nixpkgs, and use treefmt

--- flake.nix
+++ flake.nix
@@ -1,17 +1,11 @@
 {
   description = "Ema project";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/2ea2f7b6d0cb7ce0712f2aa80303cda08deb0de2";
+    nixpkgs.url = "github:nixos/nixpkgs/abfd31179174133ab8131139d650297bf4da63b7";
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.nixpkgs.follows = "nixpkgs";
     flake-compat.url = "github:edolstra/flake-compat";
     flake-compat.flake = false;
-    lint-utils = {
-      type = "git";
-      url = "https://gitlab.homotopic.tech/nix/lint-utils.git";
-      ref = "overengineered";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem
@@ -39,17 +33,9 @@
                     haskell-language-server
                     ormolu
                     pkgs.nixpkgs-fmt
+                    pkgs.treefmt
                   ]);
             };
-
-          lintSpec = {
-            nixpkgs-fmt = { };
-            cabal-fmt = { };
-            fourmolu = {
-              ghcOpts = "-o-XTypeApplications -o-XImportQualifiedPost";
-            };
-          };
-
         in
         rec {
           # Used by `nix build`
@@ -62,29 +48,6 @@
           packages = {
             default = emaProject false;
           };
-
-          # Used by `nix run ...`
-          apps = {
-            format = inputs.lint-utils.mkApp.${system} lintSpec;
-          };
-
-          # Used by `nix flake check` (but see next attribute)
-          checks = {
-            format = inputs.lint-utils.mkChecks.${system} lintSpec;
-          };
-
-          # We need this hack because `nix flake check` won't work for Haskell
-          # projects: https://nixos.wiki/wiki/Import_From_Derivation#IFD_and_Haskell
-          #
-          # Instead, run: `nix build .#check.x86_64-linux` (replace with your system)
-          check =
-            pkgs.runCommand "combined-checks"
-              {
-                checksss = builtins.attrValues self.checks.${system};
-              } ''
-              echo $checksss
-              touch $out
-            '';
 
         }) // {
       herculesCI.ciSystems = [ "x86_64-linux" ];
