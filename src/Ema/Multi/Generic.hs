9b9ca3712cda6051fcfa493c307838e5edc89882
Sridhar Ratnakumar <srid@srid.ca>
2022-06-25 12:45:13 -0400

    Add RGeneric, rfrom, rto

--- src/Ema/Multi/Generic.hs
+++ src/Ema/Multi/Generic.hs
@@ -1,23 +1,24 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
 
 -- | WIP https://github.com/srid/ema/issues/92
 module Ema.Multi.Generic where
 
-import Data.SOP (I (..), NP (..))
 import Data.SOP.Extra (NPConst (npConstFrom))
 import Ema.Multi
 import Ema.Route.Class (IsRoute (..))
 import Ema.Route.Encoder
-import Optics.Core
-
-{- |
-type family RCode (xss :: [[Type]]) :: [Type] where
-  RCode '[] = '[]
-  RCode ('[] ': rest) = Void ': RCode rest
-  RCode ('[x] ': rest) = x ': RCode rest
-  RCode (_ ': rest) = TypeError ( 'Text "MultiRoute: too many arguments")
--}
+import GHC.TypeLits
+import Generics.SOP
+import Optics.Core (
+  Iso',
+  ReversibleOptic (re),
+  coercedTo,
+  review,
+  (%),
+ )
+import Prelude hiding (All, Generic)
 
 {- | MotleyRoute is a class of routes with an underlying MultiRoute (and MultiModel) representation.
 
@@ -103,3 +104,57 @@ instance
   allRoutes m =
     WithConstModel . review motleyRouteIso
       <$> allRoutes (motleySubModels @r m)
+
+-- | Like `Generic` but for Route types only.
+class RGeneric r where
+  type RCode r :: [Type]
+  rfrom :: r -> NS I (RCode r)
+  rto :: NS I (RCode r) -> r
+
+instance (Generic r, RGeneric' (Code r), All RouteNP (Code r)) => RGeneric r where
+  type RCode r = RCode' (Code r)
+  rfrom = rfrom' @(Code r) . unSOP . from
+  rto = to . SOP . rto' @(Code r)
+
+class RGeneric' (xss :: [[Type]]) where
+  type RCode' xss :: [Type]
+  rfrom' :: NS (NP I) xss -> NS I (RCode' xss)
+  rto' :: NS I (RCode' xss) -> NS (NP I) xss
+
+instance RGeneric' '[] where
+  type RCode' '[] = '[]
+  rfrom' = \case {}
+  rto' = \case {}
+
+instance (RGeneric' xss, RouteNP xs) => RGeneric' (xs ': xss) where
+  type RCode' (xs ': xss) = RouteNPType xs ': RCode' xss
+  rfrom' = \case
+    Z routeNP -> Z $ I $ fromRouteNP routeNP
+    S rest -> S $ rfrom' @xss rest
+  rto' = \case
+    Z (I t) -> Z $ toRouteNP t
+    S rest -> S $ rto' @xss rest
+
+{- | Class of `NP` that can be used in a route tyupe.
+
+    Ensures that the constructor can have 0 or 1 products only.
+-}
+class RouteNP (xs :: [Type]) where
+  type RouteNPType xs :: Type
+  fromRouteNP :: NP I xs -> RouteNPType xs
+  toRouteNP :: RouteNPType xs -> NP I xs
+
+instance RouteNP '[] where
+  type RouteNPType '[] = ()
+  fromRouteNP Nil = ()
+  toRouteNP () = Nil
+
+instance RouteNP (x ': '[]) where
+  type RouteNPType (x ': '[]) = x
+  fromRouteNP (I x :* Nil) = x
+  toRouteNP x = I x :* Nil
+
+instance (TypeError ( 'Text "MultiRoute: too many arguments")) => RouteNP (x ': x' ': xs) where
+  type RouteNPType (x ': x' ': xs) = TypeError ( 'Text "MultiRoute: too many arguments")
+  fromRouteNP _ = undefined
+  toRouteNP _ = undefined
