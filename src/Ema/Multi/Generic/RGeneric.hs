afd45e34e45c4c74c64b08cdddc8404209449051
Sridhar Ratnakumar <srid@srid.ca>
2022-06-26 11:18:52 -0400

    Add type name and constructor names to `RGeneric` (type-level)

--- src/Ema/Multi/Generic/RGeneric.hs
+++ src/Ema/Multi/Generic/RGeneric.hs
@@ -5,16 +5,21 @@ module Ema.Multi.Generic.RGeneric where
 
 import GHC.TypeLits (ErrorMessage (Text), TypeError)
 import Generics.SOP
+import Generics.SOP.Type.Metadata qualified as SOPM
 import Prelude hiding (All, Generic)
 
 -- | Like `Generic` but for Route types only.
-class Generic r => RGeneric r where
+class (Generic r, HasDatatypeInfo r) => RGeneric r where
   type RCode r :: [Type]
+  type RDatatypeName r :: SOPM.DatatypeName
+  type RConstructorNames r :: [SOPM.ConstructorName]
   rfrom :: r -> NS I (RCode r)
   rto :: NS I (RCode r) -> r
 
-instance (Generic r, RGeneric' (Code r), All RouteNP (Code r)) => RGeneric r where
+instance (Generic r, HasDatatypeInfo r, RGeneric' (Code r), All RouteNP (Code r)) => RGeneric r where
   type RCode r = RCode' (Code r)
+  type RDatatypeName r = RDatatypeName' (DatatypeInfoOf r)
+  type RConstructorNames r = RConstructorNames' (DatatypeInfoOf r)
   rfrom = rfrom' @(Code r) . unSOP . from
   rto = to . SOP . rto' @(Code r)
 
@@ -36,6 +41,39 @@ instance (RGeneric' xss, RouteNP xs) => RGeneric' (xs ': xss) where
   rto' = \case
     Z (I t) -> Z $ toRouteNP t
     S rest -> S $ rto' @xss rest
+
+{- | Like `DatatypeInfo` but pertaining to Route types only.
+
+ The main difference is that it uses a `Maybe` instead of a `[]` to represent
+ ADT constructors in line with `RouteNP`.
+-}
+data RDatatypeInfo
+  = RADT SOPM.ModuleName SOPM.DatatypeName (Maybe SOPM.ConstructorInfo)
+  | RNewtype SOPM.ModuleName SOPM.DatatypeName SOPM.ConstructorInfo
+
+type family RDatatypeName' (info :: SOPM.DatatypeInfo) :: SOPM.DatatypeName where
+  RDatatypeName' ( 'SOPM.ADT _ name _ _) =
+    name
+  RDatatypeName' ( 'SOPM.Newtype _ name _) =
+    name
+
+type family RConstructorNames' (info :: SOPM.DatatypeInfo) :: [SOPM.ConstructorName] where
+  RConstructorNames' ( 'SOPM.ADT _ _ constrs _) =
+    GetConstructorNames constrs
+  RConstructorNames' ( 'SOPM.Newtype _ _ constr) =
+    '[GetConstructorName constr]
+
+type family GetConstructorName (info :: SOPM.ConstructorInfo) :: SOPM.ConstructorName where
+  GetConstructorName ( 'SOPM.Constructor name) =
+    name
+  GetConstructorName ( 'SOPM.Infix name _ _) =
+    name
+  GetConstructorName ( 'SOPM.Record name _) =
+    name
+
+type family GetConstructorNames (infos :: [SOPM.ConstructorInfo]) :: [SOPM.ConstructorName] where
+  GetConstructorNames '[] = '[]
+  GetConstructorNames (x ': xs) = GetConstructorName x ': GetConstructorNames xs
 
 {- | Class of `NP` that can be used in a route tyupe.
 
