047f87c3457e78d233ebf746ad34aa60aea26116
Sridhar Ratnakumar <srid@srid.ca>
2022-07-15 10:45:04 -0400

    Run treefmt

--- src/Ema/Route/Generic/TH.hs
+++ src/Ema/Route/Generic/TH.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module Ema.Route.Generic.TH (
-  deriveIsRoute
+  deriveIsRoute,
 ) where
 
-import Ema.Route.Class (IsRoute)
-import Ema.Route.Generic (GenericRoute, HasSubRoutes, HasSubModels, WithSubRoutes)
-import Language.Haskell.TH
 import Data.Proxy
+import Ema.Route.Class (IsRoute)
+import Ema.Route.Generic (GenericRoute, HasSubModels, HasSubRoutes, WithSubRoutes)
+import Language.Haskell.TH
 
-{-| @deriveIsRoute route model subroutes@ derives 'HasSubRoutes', 'HasSubModels', and 'IsRoute' for the given @route@.
+{- | @deriveIsRoute route model subroutes@ derives 'HasSubRoutes', 'HasSubModels', and 'IsRoute' for the given @route@.
 
 Subroutes are optionally supplied, but if they are then the length of the list must be the same as the number of
 constructors in @route@.
@@ -18,23 +18,28 @@ TODO: Add TypeErrors to catch mismatched 'WithSubRoutes' list shapes at the gene
 -}
 deriveIsRoute :: Name -> Name -> Maybe [Name] -> Q [Dec]
 deriveIsRoute route model subroutes = do
-  let instances = 
+  let instances =
         [ ''HasSubRoutes
         , ''HasSubModels
         , ''IsRoute
         ]
-  let opts = 
+  let opts =
         toTyList $
-          [ ConT (mkName "WithModel") `AppT` (ConT model) ]
-            <> maybe [] (\s -> [ ConT ''WithSubRoutes `AppT` toTyList (ConT <$> s) ]) subroutes
-  pure $ flip fmap instances $ \i ->
-    StandaloneDerivD 
-      (Just (ViaStrategy 
-        (ConT ''GenericRoute
-          `AppT` (ConT route)
-          `AppT` opts)))
-    []
-    (ConT i `AppT` ConT route)
+          [ConT (mkName "WithModel") `AppT` (ConT model)]
+            <> maybe [] (\s -> [ConT ''WithSubRoutes `AppT` toTyList (ConT <$> s)]) subroutes
+  pure $
+    flip fmap instances $ \i ->
+      StandaloneDerivD
+        ( Just
+            ( ViaStrategy
+                ( ConT ''GenericRoute
+                    `AppT` (ConT route)
+                    `AppT` opts
+                )
+            )
+        )
+        []
+        (ConT i `AppT` ConT route)
   where
-    toTyList (n:ns) = PromotedConsT `AppT` n `AppT` toTyList ns 
-    toTyList []     = PromotedNilT
+    toTyList (n : ns) = PromotedConsT `AppT` n `AppT` toTyList ns
+    toTyList [] = PromotedNilT
