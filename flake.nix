6a8b1b41dc567a8d84706015c3b53dc6f2531409
Sridhar Ratnakumar <srid@srid.ca>
2022-07-17 11:14:11 -0400

    nix: use nixpkgs-unstable (advances +2 days)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Ema project";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs.follows = "nixpkgs";
     haskell-flake.url = "github:srid/haskell-flake";
