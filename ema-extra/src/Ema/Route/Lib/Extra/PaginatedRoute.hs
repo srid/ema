5a46180b31d903c68b3848365848f2ab925fd3b3
Sridhar Ratnakumar <srid@srid.ca>
2022-11-19 12:58:53 -0500

    simplify

--- ema-extra/src/Ema/Route/Lib/Extra/PaginatedRoute.hs
+++ ema-extra/src/Ema/Route/Lib/Extra/PaginatedRoute.hs
@@ -4,12 +4,7 @@ module Ema.Route.Lib.Extra.PaginatedRoute (
   -- * Page
   Page,
 
-  -- * PaginatedRoute
-  PaginatedRoute (..),
-
   -- * Functions
-  getPage,
-  fromPage,
   lookupPage,
   lookupPage',
 ) where
@@ -21,59 +16,43 @@ import Ema.Route.Prism (toPrism_)
 import Generics.SOP qualified as SOP
 import Optics.Core (prism')
 
-newtype Page = Page {unPage :: Word}
-  deriving newtype (Show, Eq, Ord, Num, Enum)
-
-data PaginatedRoute (t :: Type) = PaginatedRoute_Main | PaginatedRoute_OnPage Page
-  deriving stock (Show, Eq, Ord, Generic)
+newtype Page (t :: Type) = Page {unPage :: Word}
+  deriving newtype (Show, Eq, Ord, Num, Enum, Default)
+  deriving stock (Generic)
   deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
 
-instance Default (PaginatedRoute t) where
-  def = PaginatedRoute_Main
-
-getPage :: forall {t}. PaginatedRoute t -> Page
-getPage = \case
-  PaginatedRoute_Main -> Page 1
-  PaginatedRoute_OnPage p -> p
-
-fromPage :: forall {t}. Page -> PaginatedRoute t
-fromPage = \case
-  Page 1 -> PaginatedRoute_Main
-  p -> PaginatedRoute_OnPage p
-
-lookupPage :: HasCallStack => PaginatedRoute a -> NonEmpty [a] -> [a]
+lookupPage :: HasCallStack => Page a -> NonEmpty [a] -> [a]
 lookupPage r xs =
   fromMaybe (error outOfBoundsError) $ lookupPage' r xs
   where
     outOfBoundsError =
       "lookupPage: Page "
-        <> show (getPage r)
+        <> show r
         <> " is out of bounds of total available pages: "
         <> show (length xs)
 
-lookupPage' :: PaginatedRoute a -> NonEmpty [a] -> Maybe [a]
-lookupPage' r xs =
-  toList xs !!? (fromInteger . toInteger $ unPage (getPage r) - 1)
+lookupPage' :: Page a -> NonEmpty [a] -> Maybe [a]
+lookupPage' p xs =
+  toList xs !!? (fromInteger . toInteger $ unPage p - 1)
 
-instance IsRoute (PaginatedRoute a) where
-  type RouteModel (PaginatedRoute a) = NonEmpty [a]
+instance IsRoute (Page a) where
+  type RouteModel (Page a) = NonEmpty [a]
   routePrism m =
     -- TODO: Refactor this using lens composition.
     toPrism_ $
       prism'
         ( \case
-            PaginatedRoute_Main -> "index.html"
-            PaginatedRoute_OnPage page -> "page/" <> show (unPage page) <> ".html"
+            Page 0 -> "index.html"
+            Page num -> "page/" <> show num <> ".html"
         )
         ( \fp -> do
             if fp == "index.html"
-              then pure PaginatedRoute_Main
+              then pure def
               else do
                 page <- fmap toString $ T.stripSuffix ".html" =<< T.stripPrefix "page/" (toText fp)
-                p <- Page <$> readMaybe page
-                let r = fromPage p
+                r <- Page <$> readMaybe page
                 void $ lookupPage' r m -- Check if this page exists
                 pure r
         )
   routeUniverse m =
-    fromPage <$> [1 :: Page .. (fromInteger . toInteger $ length m)]
+    [1 :: (Page a) .. (fromInteger . toInteger $ length m)]
