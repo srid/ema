f415c55c022a558a95c8a270ae94fa57a228d8c7
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2022-11-19 13:25:05 -0500

    Simplify `PaginatedRoute` (#146)
    * simplify
    
    * Fix indexing, and expose pageNum
    
    * pageNum should return Int
    
    * improv
    
    * static generation
    
    * docsv

--- ema-extra/src/Ema/Route/Lib/Extra/PaginatedRoute.hs
+++ ema-extra/src/Ema/Route/Lib/Extra/PaginatedRoute.hs
@@ -1,15 +1,14 @@
 {-# LANGUAGE DeriveAnyClass #-}
 
+-- | Provides a `Page` type that enables any route to be paginated.
 module Ema.Route.Lib.Extra.PaginatedRoute (
   -- * Page
   Page,
 
-  -- * PaginatedRoute
-  PaginatedRoute (..),
-
   -- * Functions
-  getPage,
-  fromPage,
+  pageNum,
+  fromNum,
+  pageRange,
   lookupPage,
   lookupPage',
 ) where
@@ -21,59 +20,68 @@ import Ema.Route.Prism (toPrism_)
 import Generics.SOP qualified as SOP
 import Optics.Core (prism')
 
-newtype Page = Page {unPage :: Word}
-  deriving newtype (Show, Eq, Ord, Num, Enum)
+{- | Represents a single page in a multi-page view/route.
 
-data PaginatedRoute (t :: Type) = PaginatedRoute_Main | PaginatedRoute_OnPage Page
-  deriving stock (Show, Eq, Ord, Generic)
+  Use `pageNum` to retrieve the user-facing page number which are 1-index;
+  `fromNum` to convert them back.
+-}
+newtype Page (t :: Type) = Page {unPage :: Word}
+  deriving newtype (Show, Eq, Ord, Num, Enum, Default)
+  deriving stock (Generic)
   deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
 
-instance Default (PaginatedRoute t) where
-  def = PaginatedRoute_Main
+-- | Get the user-facing page number.
+pageNum :: forall a. Page a -> Int
+pageNum (Page n) =
+  fromInteger . toInteger $ n + 1
 
-getPage :: forall {t}. PaginatedRoute t -> Page
-getPage = \case
-  PaginatedRoute_Main -> Page 1
-  PaginatedRoute_OnPage p -> p
+-- | Convert the user-facing page number.
+fromNum :: forall a. Int -> Maybe (Page a)
+fromNum n = do
+  guard $ n > 0
+  pure $ fromInteger . toInteger $ n - 1
 
-fromPage :: forall {t}. Page -> PaginatedRoute t
-fromPage = \case
-  Page 1 -> PaginatedRoute_Main
-  p -> PaginatedRoute_OnPage p
+-- | Enumerate list of all pages given the total number of pages.
+pageRange :: forall a. HasCallStack => Int -> NonEmpty (Page a)
+pageRange total =
+  fromMaybe (error "pageRange: total must be positive and non-zero") $ do
+    end <- fromNum @a total
+    nonEmpty [def .. end]
 
-lookupPage :: HasCallStack => PaginatedRoute a -> NonEmpty [a] -> [a]
+-- | Retrieve the given page from the list.
+lookupPage :: HasCallStack => Page a -> NonEmpty [a] -> [a]
 lookupPage r xs =
   fromMaybe (error outOfBoundsError) $ lookupPage' r xs
   where
     outOfBoundsError =
       "lookupPage: Page "
-        <> show (getPage r)
+        <> show r
         <> " is out of bounds of total available pages: "
         <> show (length xs)
 
-lookupPage' :: PaginatedRoute a -> NonEmpty [a] -> Maybe [a]
-lookupPage' r xs =
-  toList xs !!? (fromInteger . toInteger $ unPage (getPage r) - 1)
+lookupPage' :: Page a -> NonEmpty [a] -> Maybe [a]
+lookupPage' p xs =
+  toList xs !!? (fromInteger . toInteger $ unPage p)
 
-instance IsRoute (PaginatedRoute a) where
-  type RouteModel (PaginatedRoute a) = NonEmpty [a]
+instance IsRoute (Page a) where
+  type RouteModel (Page a) = NonEmpty [a]
   routePrism m =
     -- TODO: Refactor this using lens composition.
     toPrism_ $
       prism'
-        ( \case
-            PaginatedRoute_Main -> "index.html"
-            PaginatedRoute_OnPage page -> "page/" <> show (unPage page) <> ".html"
+        ( \page ->
+            if page == def
+              then "index.html"
+              else "page/" <> show (pageNum page) <> ".html"
         )
         ( \fp -> do
             if fp == "index.html"
-              then pure PaginatedRoute_Main
+              then pure def
               else do
                 page <- fmap toString $ T.stripSuffix ".html" =<< T.stripPrefix "page/" (toText fp)
-                p <- Page <$> readMaybe page
-                let r = fromPage p
+                r <- fromNum <=< readMaybe $ page
                 void $ lookupPage' r m -- Check if this page exists
                 pure r
         )
-  routeUniverse m =
-    fromPage <$> [1 :: Page .. (fromInteger . toInteger $ length m)]
+  routeUniverse =
+    toList . pageRange . length
