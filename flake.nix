f7651f4926d18018dd162fac95ecfdfda61e1b9e
Sridhar Ratnakumar <srid@srid.ca>
2023-01-20 13:38:11 -0500

    Use treefmt-nix flake module

--- flake.nix
+++ flake.nix
@@ -8,12 +8,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-root.url = "github:srid/flake-root";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, haskell-flake, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
-        haskell-flake.flakeModule
+        inputs.haskell-flake.flakeModule
+        inputs.flake-root.flakeModule
+        inputs.treefmt-nix.flakeModule
       ];
       perSystem = { config, pkgs, ... }: {
         # This attr is provided by https://github.com/srid/haskell-flake
@@ -27,19 +31,34 @@
                 });
             in
             {
-              inherit (pkgs)
-                treefmt
-                nixpkgs-fmt;
-              inherit (pkgs.haskellPackages)
-                cabal-fmt;
-              inherit (hp)
-                fourmolu;
+              treefmt = config.treefmt.build.wrapper;
               ghcid = fixCyclicReference hp.ghcid;
               haskell-language-server = hp.haskell-language-server.overrideScope (lself: lsuper: {
                 ormolu = fixCyclicReference hp.ormolu;
               });
             };
           overrides = self: super: with pkgs.haskell.lib; { };
+        };
+
+        # treefmt-nix configuration
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+          package = pkgs.treefmt;
+
+          programs.ormolu.enable = true;
+          programs.nixpkgs-fmt.enable = true;
+          programs.cabal-fmt.enable = true;
+
+          # We use fourmolu
+          programs.ormolu.package = pkgs.haskellPackages.fourmolu;
+          settings.formatter.ormolu = {
+            options = [
+              "--ghc-opt"
+              "-XImportQualifiedPost"
+              "--ghc-opt"
+              "-XTypeApplications"
+            ];
+          };
         };
       };
 
