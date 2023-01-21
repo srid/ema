55f338f4d72e0413c876b04922705ce575fca21d
Sridhar Ratnakumar <srid@srid.ca>
2023-01-21 11:42:57 -0500

    nix: Remove cache

--- flake.nix
+++ flake.nix
@@ -1,9 +1,5 @@
 {
   description = "Ema project";
-  nixConfig = {
-    extra-substituters = "https://cache.srid.ca";
-    extra-trusted-public-keys = "cache.srid.ca:8sQkbPrOIoXktIwI0OucQBXod2e9fDjjoEZWn8OXbdo=";
-  };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
